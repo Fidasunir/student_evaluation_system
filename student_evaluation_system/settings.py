57561afe695a2abc8273fcf639077c2578e42ed4
Fida Sunir <fidasunir203@gmail.com>
2026-03-23 14:14:48 +0530

    Initial commit

--- student_evaluation_system/settings.py
+++ student_evaluation_system/settings.py
@@ -151,7 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 import google.generativeai as genai
 
 # Replace with your actual API key
-GEMINI_API_KEY = "AIzaSyBEL5S1WXfwl5hHjtmlHMbj08OgHXwX-Uc"
+GEMINI_API_KEY = ""
 
 
 # settings.py
